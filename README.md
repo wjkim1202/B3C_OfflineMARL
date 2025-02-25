646ea662e37b55bdf39c11cb6af1cbeded73a57d
wjkim <kwj123123@gmail.com>
2025-02-24 20:11:02 -0500

    update

--- README.md
+++ README.md
@@ -1,43 +1,14 @@
-# Offline Multi-Agent Reinforcement Learning with Implicit Global-to-Local Value Regularization (NeurIPS 2023)
-The official implementation of "[Offline Multi-Agent Reinforcement Learning with Implicit Global-to-Local Value Regularization](https://arxiv.org/abs/2307.11620)". OMIGA provides a principled framework to convert global-level value regularization into equivalent implicit local value regularizations and simultaneously enables in-sample learning, thus elegantly bridging multi-agent value decomposition and policy learning with offline regularizations. This repository is inspired by the [TRPO-in-MARL](https://github.com/cyanrain7/TRPO-in-MARL) library for online Multi-Agent RL.
+# B3C: A Minimalist Approach to Offline Multi-Agent Reinforcement Learning
 
-**This repo provides the implementation of OMIGA in Multi-agent MuJoCo and SMAC**
-
-## Branches Overview
-| Branch name 	| Usage 	|
-|:---:	|:---:	|
-| [master](https://github.com/ZhengYinan-AIR/OMIGA) 	| OMIGA implementation for ``Multi-agent MujoCo``. |
-| [SMAC](https://github.com/ZhengYinan-AIR/OMIGA/tree/SMAC) 	| OMIGA implementation for ``SMAC``. 	|
+This repository is the implementation of "[B3C: A Minimalist Approach to Offline Multi-Agent Reinforcement Learning](https://arxiv.org/pdf/2501.18138)". 
 
 
-## Installation
-``` Bash
-conda create -n env_name python=3.9
-conda activate OMIGA
-git clone https://github.com/ZhengYinan-AIR/OMIGA.git
-cd OMIGA
-pip install -r requirements.txt
-```
+This repository is based on the implementations of [OMIGA](https://github.com/ZhengYinan-AIR/OMIGA) for multi-agent Mujoco environments and [CFCQL](https://github.com/thu-rllab/CFCQL) for multi-agent particle environments.
+
 
 ## How to run
-
-Before running the code, you need to download the necessary offline datasets ([Download link](https://cloud.tsinghua.edu.cn/d/dcf588d659214a28a777/)). Then, make sure the config file at [configs/config.py](https://github.com/ZhengYinan-AIR/Offline-MARL/blob/master/configs/config.py) is correct. Set the **data_dir** parameter as the storage location for the downloaded data, and configure parameters **scenario**, **agent_conf**, and **data_type**. You can run the code as follows:
 ``` Bash
-# If the location of the dataset is at: "/data/Ant-v2-2x4-expert.hdf5"
-cd OMIGA
-python run_mujoco.py --data_dir="/data/" --scenario="Ant-v2" --agent_conf="2x4" --data_type="expert"
+python run_mujoco.py --seed 0 --coeff_rl 1 --clipq 1 --bc 1 --mixer 'nonmono' --offline_ver 2 --scenario 'HalfCheetah-v2' --agent_conf '6x1' --algo 'FACMAC' --agent_obsk -100 --full_observability 1
 ```
 
 
-
-
-## Bibtex
-If you find our code and paper can help, please cite our paper as:
-```
-@article{wang2023offline,
-  title={Offline Multi-Agent Reinforcement Learning with Implicit Global-to-Local Value Regularization},
-  author={Wang, Xiangsen and Xu, Haoran and Zheng, Yinan and Zhan, Xianyuan},
-  journal={Advances in Neural Information Processing Systems},
-  year={2023}
-}
-```
--- run.sh
+++ run.sh
@@ -1,50 +1,26 @@
 
-#python run_mujoco.py --scenario 'Hopper-v2' --agent_conf '3x1' --data_type 'expert' --algo 'OMIGA'
-#python run_mujoco.py --scenario 'Hopper-v2' --agent_conf '3x1' --data_type 'expert' --algo 'FACMAC' --clipq 1 --comm 0
-#python run_mujoco.py --scenario 'Hopper-v2' --agent_conf '3x1' --data_type 'expert' --algo 'FACMAC' --clipq 1 --comm 1
-#
-#python run_mujoco.py --scenario 'Hopper-v2' --agent_conf '3x1' --data_type 'medium' --algo 'OMIGA'
-#python run_mujoco.py --scenario 'Hopper-v2' --agent_conf '3x1' --data_type 'medium' --algo 'FACMAC' --clipq 1 --comm 0
-#python run_mujoco.py --scenario 'Hopper-v2' --agent_conf '3x1' --data_type 'medium' --algo 'FACMAC' --clipq 1 --comm 1
-#
-#python run_mujoco.py --scenario 'Hopper-v2' --agent_conf '3x1' --data_type 'medium-expert' --algo 'OMIGA'
-#python run_mujoco.py --scenario 'Hopper-v2' --agent_conf '3x1' --data_type 'medium-expert' --algo 'FACMAC' --clipq 1 --comm 0
-#python run_mujoco.py --scenario 'Hopper-v2' --agent_conf '3x1' --data_type 'medium-expert' --algo 'FACMAC' --clipq 1 --comm 1
-#
-# python run_mujoco.py --scenario 'Hopper-v2' --agent_conf '3x1' --data_type 'medium-replay' --algo 'OMIGA'
-#python run_mujoco.py --scenario 'Hopper-v2' --agent_conf '3x1' --data_type 'medium-replay' --algo 'FACMAC' --clipq 1 --comm 0
-#python run_mujoco.py --scenario 'Hopper-v2' --agent_conf '3x1' --data_type 'medium-replay' --algo 'FACMAC' --clipq 1 --comm 1
+
+############### Partially observable Mujoco envs
+############### --scenario 'HalfCheetah-v2' --agent_conf '6x1' --agent_obsk 0 --full_observability 0 --offline_ver 0/1/2/3/4/5 (e/m1/m2/e-m1/e-m2/m1-m2) 
+############### --scenario 'HalfCheetah-v2' --agent_conf '6x1' --agent_obsk 1 --full_observability 0 --offline_ver 0/1/2/3/4/5 (e/m1/m2/e-m1/e-m2/m1-m2) 
+############### --scenario 'manyagent_swimmer' --agent_conf '5x2' --agent_obsk 0 --full_observability 0 --offline_ver 0/1/2/3 (e/m1/m2/e-m1/e-m2/m1-m2)  
+
+python run_mujoco.py --seed 0 --coeff_rl 1 --clipq 1 --bc 1 --mixer 'nonmono' \
+    --offline_ver 5 --scenario 'HalfCheetah-v2' --agent_conf '6x1' --algo 'FACMAC' --agent_obsk 0 --full_observability 0
+# python run_mujoco.py --seed 0 --coeff_rl 1 --clipq 1 --bc 1 --mixer 'nonmono' \
+#    --offline_ver 5 --scenario 'HalfCheetah-v2' --agent_conf '6x1' --algo 'FACMAC' --agent_obsk 1 --full_observability 0
 
 
 
-#python run_mujoco.py --scenario 'Ant-v2' --agent_conf '2x4' --data_type 'expert' --algo 'OMIGA' --seed 0
-#python run_mujoco.py --scenario 'Ant-v2' --agent_conf '2x4' --data_type 'expert' --algo 'FACMAC' --clipq 1 --comm 0 --seed 0
-#python run_mujoco.py --scenario 'Ant-v2' --agent_conf '2x4' --data_type 'expert' --algo 'FACMAC' --clipq 1 --comm 1 --seed 0
-#
-#python run_mujoco.py --scenario 'Ant-v2' --agent_conf '2x4' --data_type 'medium-expert' --algo 'OMIGA'
-#python run_mujoco.py --scenario 'Ant-v2' --agent_conf '2x4' --data_type 'medium-expert' --algo 'FACMAC' --clipq 1 --comm 0 --seed 0
-#python run_mujoco.py --scenario 'Ant-v2' --agent_conf '2x4' --data_type 'medium-expert' --algo 'FACMAC' --clipq 1 --comm 1 --seed 0
-#
+############### Fully observable Mujoco envs
+############### --scenario 'HalfCheetah-v2' --agent_conf '6x1' --full_observability 1 --offline_ver 0/1/2/3 (expert/medium-expert/medium/medium-replay)
+############### --scenario 'Hopper-v2' --agent_conf '3x1' --full_observability 1 --offline_ver 0/1/2/3 (expert/medium-expert/medium/medium-replay)
+############### --scenario 'Ant-v2' --agent_conf '2x4' --full_observability 1 --offline_ver 0/1/2/3 (expert/medium-expert/medium/medium-replay)
 
-#python run_mujoco.py --scenario 'Ant-v2' --agent_conf '2x4' --data_type 'medium' --algo 'OMIGA' --seed 0
-#python run_mujoco.py --scenario 'Ant-v2' --agent_conf '2x4' --data_type 'medium' --algo 'FACMAC' --clipq 1 --comm 0 --seed 0
-#python run_mujoco.py --scenario 'Ant-v2' --agent_conf '2x4' --data_type 'medium' --algo 'FACMAC' --clipq 1 --comm 1 --seed 0
+# python run_mujoco.py --seed 0 --coeff_rl 1 --clipq 1 --bc 1 --mixer 'nonmono' \
+#     --offline_ver 2 --scenario 'HalfCheetah-v2' --agent_conf '6x1' --algo 'FACMAC' --agent_obsk -100 --full_observability 1
+# python run_mujoco.py --seed 0 --coeff_rl 1 --clipq 1 --bc 1 --mixer 'nonmono' \
+#     --offline_ver 2 --scenario 'Hopper-v2' --agent_conf '3x1' --algo 'FACMAC' --agent_obsk -100 --full_observability 1
+# python run_mujoco.py --seed 0 --coeff_rl 1 --clipq 1 --bc 1 --mixer 'nonmono' \
+#     --offline_ver 2 --scenario 'Ant-v2' --agent_conf '2x4' --algo 'FACMAC' --agent_obsk -100 --full_observability 1
 
-
-python run_mujoco.py --scenario 'Ant-v2' --agent_conf '2x4' --data_type 'medium-replay' --algo 'OMIGA' --seed 0
-python run_mujoco.py --scenario 'Ant-v2' --agent_conf '2x4' --data_type 'medium-replay' --algo 'FACMAC' --clipq 1 --comm 0 --seed 0
-python run_mujoco.py --scenario 'Ant-v2' --agent_conf '2x4' --data_type 'medium-replay' --algo 'FACMAC' --clipq 1 --comm 1 --seed 0
-
-
-
-# expert    medium-expert    medium   medium-replay
-
-
-
-
-# python run_mujoco.py --comm 0 --offline_ver 3 --algo 'FACMAC' --clipq 1
-# python run_mujoco.py --comm 1 --offline_ver 3 --algo 'FACMAC' --clipq 1
-# python run_mujoco.py --comm 0 --offline_ver 4 --algo 'FACMAC' --clipq 1
-# python run_mujoco.py --comm 1 --offline_ver 4 --algo 'FACMAC' --clipq 1
-# python run_mujoco.py --comm 0 --offline_ver 2 --algo 'FACMAC' --clipq 1
-# python run_mujoco.py --comm 1 --offline_ver 2 --algo 'FACMAC' --clipq 1
