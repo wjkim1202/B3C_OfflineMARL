5d42da12b0be689e7900de7600991788b14bf2a8
wjkim <kwj123123@gmail.com>
2025-02-24 20:03:57 -0500

    Initial commit for new repository

--- runpp8.sh
+++
@@ -1,8 +0,0 @@
-python run_mujoco.py --offline_ver 1 --seed 0 --clipq 1 --comm 0 --pg_norm 8 --mixer 'vdn' --agent_view_radius 0.5 --algo 'FACMAC' --env_name 'particle' --scenario 'continuous_pred_prey_3a' --total_iterations 1000000
-python run_mujoco.py --offline_ver 1 --seed 1 --clipq 1 --comm 0 --pg_norm 8 --mixer 'vdn' --agent_view_radius 0.5 --algo 'FACMAC' --env_name 'particle' --scenario 'continuous_pred_prey_3a' --total_iterations 1000000
-python run_mujoco.py --offline_ver 1 --seed 2 --clipq 1 --comm 0 --pg_norm 8 --mixer 'vdn' --agent_view_radius 0.5 --algo 'FACMAC' --env_name 'particle' --scenario 'continuous_pred_prey_3a' --total_iterations 1000000
-
-python run_mujoco.py --offline_ver 3 --seed 0 --clipq 1 --comm 0 --pg_norm 8 --mixer 'vdn' --agent_view_radius 0.5 --algo 'FACMAC' --env_name 'particle' --scenario 'continuous_pred_prey_3a' --total_iterations 1000000
-python run_mujoco.py --offline_ver 3 --seed 1 --clipq 1 --comm 0 --pg_norm 8 --mixer 'vdn' --agent_view_radius 0.5 --algo 'FACMAC' --env_name 'particle' --scenario 'continuous_pred_prey_3a' --total_iterations 1000000
-python run_mujoco.py --offline_ver 3 --seed 2 --clipq 1 --comm 0 --pg_norm 8 --mixer 'vdn' --agent_view_radius 0.5 --algo 'FACMAC' --env_name 'particle' --scenario 'continuous_pred_prey_3a' --total_iterations 1000000
-
